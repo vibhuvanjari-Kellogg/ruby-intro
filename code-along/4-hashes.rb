7ebf422f78bdc6e28cb4d3dd895a1aca8f5e8523
Vibhu Vanjari <vibhu.vanjari@kellogg.northwestern.edu>
2025-01-28 21:04:23 +0000

    ran through hashes

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -3,7 +3,26 @@
 # ruby 4-hashes.rb
 
 # Hashes are lists of *key-value pairs*
+me = {"name" => "Vibhu", "location" => "Evanston", "status" => "busy"}
+puts me
+p me
 
 # Accessing data from the hash
 
+location = me["location"]
+puts location
+
 # More Complex Hashes
+
+my_profile = {
+    "name" => "Vibhu",
+    "location" => {"city" => "Evanston", "state" => "IL"},
+    "status" => "busy"
+}
+
+puts my_profile
+puts my_profile["location"]
+puts my_profile["location"]["state"]
+
+my_profile["name"] = "Vibhu Vanjari"
+puts my_profile
