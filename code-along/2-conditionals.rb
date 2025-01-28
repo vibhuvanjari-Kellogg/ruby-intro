d4a849e931cf252b0d4d651ab1f234e760953c7b
Vibhu Vanjari <vibhu.vanjari@kellogg.northwestern.edu>
2025-01-28 20:28:52 +0000

    ran through conditionals

--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -3,13 +3,56 @@
 # ruby 2-conditionals.rb
 
 # Booleans
+test_is_true = true
+puts test_is_true
+
+test_is_false = false
+puts test_is_false
 
 # Boolean Expressions
+puts 3 == 2
+puts 3 == 3
+puts 3 != 2
+puts 3 > 2 
 
 # If Conditional Logic
+if 3 == 2
+    puts "This test should never display."
+end
+
+if 3 > 2
+    puts "This test should always display."
+end
 
 # If/Else Conditional Logic
 
+user_entered_password = "tacos"
+real_password = "secret"
+
+if user_entered_password == real_password
+    puts "match"
+else
+    puts "incorrect"
+end
+
 # Elsif Conditional Logic
 
+your_team_score = 2
+other_team_score = 3
+
+if your_team_score > other_team_score
+    puts "you win"
+elsif your_team_score < other_team_score
+    puts "you lose"
+else
+    puts "tie"
+end
+
 # Combining Expressions
+
+temp = 68
+precipitation = 0
+
+if temp >= 65 && temp <= 75 && precipitation == 0
+    puts "It's perfect outside"
+end
